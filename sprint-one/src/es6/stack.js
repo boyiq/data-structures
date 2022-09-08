d9073e0a894082b06e7d2616e1eb5b46f0b95559
avaboonzaayer <sam.boonzaayer@gmail.com>
2022-09-08 16:54:35 -0700

    pass data-structures sprint one

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,26 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    let lastIndex = this.size();
+    this.storage[lastIndex + 1] = value;
+  }
+
+  pop () {
+    let lastIndex = this.size();
+    let lastElement = this.storage[lastIndex];
+    delete this.storage[lastIndex];
+    return lastElement;
+  }
+
+  size () {
+    let size = 0;
+    for (let key in this.storage) {
+      size++;
+    }
+    return size;
+  }
 }
