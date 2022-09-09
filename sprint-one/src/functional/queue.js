72cc12ee69a333c61ac1957514e290da62ad3ed8
Boyi Qu <boyiqu9596@gmail.com>
2022-09-08 22:45:59 -0700

    completed functional and functional share queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -1,19 +1,48 @@
 var Queue = function() {
   var someInstance = {};
-
+  someInstance.head = null;
+  someInstance.tail = null;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    if (someInstance.head === null) {
+      someInstance.head = 0;
+      someInstance.tail = 0;
+    } else {
+      someInstance.tail ++;
+    }
+    let key = someInstance.tail;
+    storage[key] = value;
   };
 
   someInstance.dequeue = function() {
+    let itemDequeued = storage[someInstance.head];
+    delete storage[someInstance.head];
+    if (someInstance.tail > 0) {
+      for (let key in storage) {
+        let newkey = JSON.stringify(Number(key) - 1);
+        storage[newkey] = storage[key];
+        delete storage[key];
+        someInstance.tail --;
+      }
+    } else if (someInstance.tail === 0) {
+      someInstance.tail = null;
+      someInstance.head = null;
+    }
+    return itemDequeued;
   };
 
   someInstance.size = function() {
+    if (someInstance.tail === null) {
+      return 0;
+    } else {
+      return someInstance.tail + 1;
+    }
   };
 
   return someInstance;
 };
+
